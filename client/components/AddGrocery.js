da62525dbfa12123b77d24663a75e449752e0158
FCraven <craven.francis@gmail.com>
2019-02-11 20:06:15 -0500

    created Footer, GroceryItem, GroceryList, AddGrocery components

---
+++ client/components/AddGrocery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddGrocery = (props) => {
+  return(
+    <div>
+      <h1>AddGrocery</h1>
+    </div>
+  )
+}
+
+export default AddGrocery
---
+++ client/components/GroceryItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GroceryItem =(props)=> {
+  return (
+    <div>
+      <h1>GroceryItem</h1>
+    </div>
+  )
+}
+
+
+export default GroceryItem
